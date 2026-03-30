71d88b60b306b1d705aabe8e51c21243db646032
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-30 10:13:31 +0530

    Java-8:Streams

---
+++ src/CoreJava/Streams/Intro.java
@@ -0,0 +1,18 @@
+package CoreJava.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Intro {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,7,8,9,11);
+
+        System.out.println(list.stream().filter(x -> x%2==0).filter(x -> x>4).findAny());
+
+        String[] strings = {"Ashu", "Sohu", "Nandu","Ashu", "Sohu", "Nandu","Ashu", "Sohu", "Nandu"};
+        Stream<String> stringStream = Arrays.stream(strings);
+        Stream<String> uStream = stringStream.filter(s -> s.endsWith("u")).distinct();
+        System.out.println(uStream.count());
+    }
+}
