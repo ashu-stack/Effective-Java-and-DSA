6778fbbfb264508ec2b77ee0f7c4ba35469edf3a
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-04-16 15:38:36 +0530

    DSA : MCM- Memoization

--- src/DynamicProgramming/MCM/MatrixChainMultiplication.java
+++ src/DynamicProgramming/MCM/MatrixChainMultiplication.java
@@ -1,26 +1,36 @@
 package DynamicProgramming.MCM;
-
+import java.util.Arrays;
 public class MatrixChainMultiplication {
     public static void main(String[] args) {
         int[] arr = {40,20,30,10,30};
         int n = arr.length;
         int i=1, j=n-1;
 
-        int cost = solveMatrix(arr,i,j);
+        int[][] dp = new int[101][101];
+
+        for (int[] ints : dp) {
+            Arrays.fill(ints, -1);
+        }
+
+        int cost = solveMatrix(arr,dp,i,j);
         System.out.println(cost);
     }
 
-    private static int solveMatrix(int[] arr, int i, int j) {
+    private static int solveMatrix(int[] arr,int[][] dp, int i, int j) {
         if(i >= j){
             return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
         int min = Integer.MAX_VALUE;
         for(int k=i; k<=j-1;k++){
-            int tempAns = solveMatrix(arr,i,k) + solveMatrix(arr, k+1, j)
+            int tempAns = solveMatrix(arr,dp,i,k) + solveMatrix(arr, dp,k+1, j)
                         + arr[i-1] * arr[k] * arr[j];
             min = Math.min(tempAns,min);
         }
-        return min;
+        dp[i][j] = min;
+        return dp[i][j];
     }
 }
